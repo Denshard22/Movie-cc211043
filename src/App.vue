51785bafcbf7b3ee2d1b0695068f45990f6586dd
Denshard22 <125809455+Denshard22@users.noreply.github.com>
2023-02-19 17:35:32 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1><span>Movie</span>App </h1>
+    </router-link>
+  </header>
+ <main>
+  <router-view />
+ </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Fira Sans', sans-serif;
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    &::selection{
+      background: transparentize(#42b883, 0.5);
     }
   }
-}
+
+
+  body{
+    background-color: #35495e;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  header{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding:  10px 16px;
+    background-color:#263443 ;
+    box-shadow: 0px 0px 6px rbg(0, 0, 0, 0.1) ;
+  }
+
+  h1{
+    color: white;
+    font-size: 28px;
+
+  }
+
+  span{
+    color: #42b883;
+  }
 </style>
+
